80eb952219fa8a861a47bea695cc80e39f125cec
tirzoJR <tirzomartinez2003@gmail.com>
2024-03-19 13:37:56 -0600

    Se agrega el Proyecto

---
+++ src/main/java/com/osdev/nevillemethod/NevilleMethod.java
@@ -0,0 +1,19 @@
+package com.osdev.nevillemethod;
+
+import com.osdev.nevillemethod.ui.MainForm;
+
+/**
+ *
+ * @author a1den
+ */
+public class NevilleMethod {
+
+    public static void main(String[] args) {
+
+        MainForm mf = new MainForm();
+        mf.setTitle("Metodo de Neville");
+        mf.setVisible(true);
+        mf.setLocationRelativeTo(null);
+
+    }
+}
